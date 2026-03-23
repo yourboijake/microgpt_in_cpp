88579d67be3f4e3aab85d48f82ecfd21611b0c87
yourboijake <jdunning@tibersolutions.com>
2026-03-22 19:29:17 -0600

    implemented tokenizer at char level

--- dataset.cpp
+++ dataset.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <unordered_map>
+#include <numeric>
+#include <algorithm>
 
 std::vector<std::string> read_dataset(std::string fpath) {
   std::vector<std::string> data;
@@ -20,11 +23,28 @@ std::vector<std::string> read_dataset(std::string fpath) {
   return data;
 }
 
+std::unordered_map<char, int> tokenize(std::vector<std::string> v) {
+  auto concat = std::accumulate(v.begin(), v.end(), std::string(""));
+  std::sort(concat.begin(), concat.end());
 
+  std::unordered_map<char, int> um;
+  int count = 0;
+  for (const char c : concat) {
+    if (um.find(c) == um.end()) {
+      um[c] = count;
+      count++;
+    }
+  }
+
+  return um;
+} 
 
 int main() {
   auto data = read_dataset("input.txt");
   std::cout << data.size() << " " << data[0] << std::endl;
+
+  auto um = tokenize(data);
+  std::cout << um['a'] << std::endl;
 
   return 0;
 }
