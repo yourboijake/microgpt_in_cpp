5628b0ea5aab8f5712a4558f887d1bfaca81197c
jacob dunning <jakeydunning98@gmail.com>
2026-04-10 19:51:20 -0600

    fixed compiler errors

--- value.cpp
+++ value.cpp
@@ -1,31 +1,34 @@
-#include <iostream>
-#include <vector>
+#include "value.h"
+#include <algorithm>
 #include <cmath>
 #include <unordered_set>
-#include <algorithm>
-#include "value.h"
+#include <vector>
 
-Value::Value(float dt, std::vector<std::shared_ptr<Value>> chn, std::vector<float> lgs) {
-    data = dt;
-    grad = 0;
-    children = chn;
-    local_grads = lgs;
-  }
+Value::Value(float dt, std::vector<std::shared_ptr<Value>> chn,
+             std::vector<float> lgs) {
+  data = dt;
+  grad = 0;
+  children = chn;
+  local_grads = lgs;
+}
 
-std::shared_ptr<Value> Value::add (std::shared_ptr<Value> a, std::shared_ptr<Value> b) {
+std::shared_ptr<Value> Value::add(std::shared_ptr<Value> a,
+                                  std::shared_ptr<Value> b) {
   float sum = a->data + b->data;
-  auto chn = {a, b};
-  auto lgs = {1.0, 1.0};
+  std::vector<std::shared_ptr<Value>> chn = {a, b};
+  std::vector<float> lgs = {1.0, 1.0};
   auto out = std::make_shared<Value>(sum, chn, lgs);
   return out;
 }
 
-std::shared_ptr<Value> Value::sub(std::shared_ptr<Value> a, std::shared_ptr<Value> b) {
+std::shared_ptr<Value> Value::sub(std::shared_ptr<Value> a,
+                                  std::shared_ptr<Value> b) {
   auto n = Value::neg(b);
   return Value::add(a, n);
 }
 
-std::shared_ptr<Value> Value::mult(std::shared_ptr<Value> a, std::shared_ptr<Value> b) {
+std::shared_ptr<Value> Value::mult(std::shared_ptr<Value> a,
+                                   std::shared_ptr<Value> b) {
   float prod = a->data * b->data;
   auto chn = {a, b};
   auto lgs = {b->data, a->data};
@@ -33,7 +36,8 @@ std::shared_ptr<Value> Value::mult(std::shared_ptr<Value> a, std::shared_ptr<Val
   return out;
 }
 
-std::shared_ptr<Value> Value::div(std::shared_ptr<Value> a, std::shared_ptr<Value> b) {
+std::shared_ptr<Value> Value::div(std::shared_ptr<Value> a,
+                                  std::shared_ptr<Value> b) {
   auto inv = Value::pow(b, -1.0f);
   return Value::mult(a, inv);
 }
@@ -50,7 +54,7 @@ std::shared_ptr<Value> Value::pow(std::shared_ptr<Value> v, float exp) {
 std::shared_ptr<Value> Value::log(std::shared_ptr<Value> v) {
   float data = std::log(v->data);
   auto chn = {v};
-  auto lgs = {1/v->data};
+  auto lgs = {1 / v->data};
   auto out = std::make_shared<Value>(data, chn, lgs);
   return out;
 }
@@ -84,67 +88,34 @@ std::vector<std::shared_ptr<Value>> Value::get_children() const {
   return this->children;
 }
 
-std::vector<float> Value::get_local_grads() const {
-  return this->local_grads;
-}
+std::vector<float> Value::get_local_grads() const { return this->local_grads; }
 
 void Value::backward(std::shared_ptr<Value> root) {
   std::vector<std::shared_ptr<Value>> topo = {};
   std::unordered_set<std::shared_ptr<Value>> visited = {};
-  void build_topo(std::shared_ptr<Value> v, std::vector<std::shared_ptr<Value>> t, std::unordered_set<std::shared_ptr<Value>> vis);
+  void build_topo(std::shared_ptr<Value> v,
+                  std::vector<std::shared_ptr<Value>> t,
+                  std::unordered_set<std::shared_ptr<Value>> vis);
   build_topo(root, topo, visited);
   root->grad = 1.0f;
   std::reverse(topo.begin(), topo.end());
-  for (std::shared_ptr<Value> v: topo) {
+  for (std::shared_ptr<Value> v : topo) {
     auto children = v->get_children();
     auto lgs = v->get_local_grads();
-    for (int i = 0; i < children.size(); i++) {
+    for (size_t i = 0; i < children.size(); i++) {
       children[i]->grad += lgs[i] * v->grad;
     }
   }
 }
 
-void build_topo(std::shared_ptr<Value> v, std::vector<std::shared_ptr<Value>> topo, std::unordered_set<std::shared_ptr<Value>> visited) {
+void build_topo(std::shared_ptr<Value> v,
+                std::vector<std::shared_ptr<Value>> topo,
+                std::unordered_set<std::shared_ptr<Value>> visited) {
   if (visited.find(v) == visited.end()) {
     visited.insert(v);
-    for (const std::shared_ptr<Value> child : v->get_children()) {
+    for (const std::shared_ptr<Value> &child : v->get_children()) {
       return build_topo(child, topo, visited);
     }
     topo.push_back(v);
   }
 }
-
-/*
-int main() {
-  Value v1 = {10};
-  Value v2 = {20};
-  auto v3 = v1 + v2;
-  std::cout << v1.data << std::endl;
-  std::cout << &v1 << std::endl;
-  std::cout << v2.data << std::endl;
-  std::cout << v3.data << std::endl;
-
-  auto ch = v3.get_children();
-  std::cout << ch[0] << " " << ch[0]->data << std::endl;
-
-  auto v4 = v1 * v2;
-  std::cout << v4.data << std::endl;
-
-  auto v5 = v1.pow(2);
-  std::cout << v5.data << std::endl;
-
-  auto v6 = v1.log();
-  std::cout << v6.data << std::endl;
-
-  auto v7 = v1.neg();
-  std::cout << v7.data << std::endl;
-
-  auto v8 = v2 / v1;
-  std::cout << v8.data << std::endl;
-
-  auto v9 = v2 - v1;
-  std::cout << v9.data << std::endl;
-
-  return 0;
-}
-*/
--- value.h
+++ value.h
@@ -1,42 +1,49 @@
 #pragma once
 
-#include <unordered_set>
 #include <memory>
+#include <unordered_set>
+#include <vector>
 
 class Value {
 
-  private:
-    std::vector<std::shared_ptr<Value>> children;
-    std::vector<float> local_grads;
+private:
+  std::vector<std::shared_ptr<Value>> children;
+  std::vector<float> local_grads;
 
-  public:
-    float data;
-    float grad;
-    Value(float dt, std::vector<std::shared_ptr<Value>> chn = {}, std::vector<float> lgs = {});
-    
-    static std::shared_ptr<Value> add(std::shared_ptr<Value> a, std::shared_ptr<Value> b);
-    
-    static std::shared_ptr<Value> sub(std::shared_ptr<Value> a, std::shared_ptr<Value> b);
-    
-    static std::shared_ptr<Value> mult(std::shared_ptr<Value> a, std::shared_ptr<Value> b);
-    
-    static std::shared_ptr<Value> div(std::shared_ptr<Value> a, std::shared_ptr<Value> b);
-    
-    static std::shared_ptr<Value> pow(std::shared_ptr<Value> v, float exp);
-    
-    static std::shared_ptr<Value> log(std::shared_ptr<Value> v);
-    
-    static std::shared_ptr<Value> exp(std::shared_ptr<Value> v);
-    
-    static std::shared_ptr<Value> relu(std::shared_ptr<Value> v);
-    
-    static std::shared_ptr<Value> neg(std::shared_ptr<Value> v);
-    
-    static void backward(std::shared_ptr<Value>);
-    
-    std::vector<std::shared_ptr<Value>> get_children() const;
-    
-    std::vector<float> get_local_grads() const;
+public:
+  float data;
+  float grad;
+  Value(float dt, std::vector<std::shared_ptr<Value>> chn = {},
+        std::vector<float> lgs = {});
+
+  static std::shared_ptr<Value> add(std::shared_ptr<Value> a,
+                                    std::shared_ptr<Value> b);
+
+  static std::shared_ptr<Value> sub(std::shared_ptr<Value> a,
+                                    std::shared_ptr<Value> b);
+
+  static std::shared_ptr<Value> mult(std::shared_ptr<Value> a,
+                                     std::shared_ptr<Value> b);
+
+  static std::shared_ptr<Value> div(std::shared_ptr<Value> a,
+                                    std::shared_ptr<Value> b);
+
+  static std::shared_ptr<Value> pow(std::shared_ptr<Value> v, float exp);
+
+  static std::shared_ptr<Value> log(std::shared_ptr<Value> v);
+
+  static std::shared_ptr<Value> exp(std::shared_ptr<Value> v);
+
+  static std::shared_ptr<Value> relu(std::shared_ptr<Value> v);
+
+  static std::shared_ptr<Value> neg(std::shared_ptr<Value> v);
+
+  static void backward(std::shared_ptr<Value>);
+
+  std::vector<std::shared_ptr<Value>> get_children() const;
+
+  std::vector<float> get_local_grads() const;
 };
 
-void build_topo(Value v, std::vector<Value> topo, std::unordered_set<Value*> visited);
+void build_topo(Value v, std::vector<Value> topo,
+                std::unordered_set<Value *> visited);
